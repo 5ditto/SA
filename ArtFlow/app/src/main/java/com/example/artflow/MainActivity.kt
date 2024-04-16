06c0c35a6597e2adf5cab594ed26fe8b25b5b106
Beatriz Monteiro <73402868+5ditto@users.noreply.github.com>
2024-04-16 19:25:00 +0100

    Já guarda no firebase

--- ArtFlow/app/src/main/java/com/example/artflow/MainActivity.kt
+++ ArtFlow/app/src/main/java/com/example/artflow/MainActivity.kt
@@ -1,32 +1,30 @@
+package com.example.artflow
+
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.artflow.AccelerometerSensorListener
-import com.example.artflow.GyroscopeSensorListener
-import com.example.artflow.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sensorDataCollector: SensorDataCollector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
-
-        if (accelerometer != null) {
-            val accelerometerSensorListener = AccelerometerSensorListener()
-            accelerometerSensorListener.setSensorManager(sensorManager)
-            sensorManager.registerListener(accelerometerSensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
-        }
-
-        if (gyroscope != null) {
-            val gyroscopeSensorListener = GyroscopeSensorListener()
-            gyroscopeSensorListener.setSensorManager(sensorManager)
-            sensorManager.registerListener(gyroscopeSensorListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL)
-        }
+        sensorDataCollector = SensorDataCollector(sensorManager)
     }
+
+    override fun onStart() {
+        super.onStart()
+        sensorDataCollector.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorDataCollector.stop()
+    }
+
 }
--- ArtFlow/app/src/main/java/com/example/artflow/SensorDataCollector.kt
+++ ArtFlow/app/src/main/java/com/example/artflow/SensorDataCollector.kt
@@ -5,13 +5,17 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
+import com.google.firebase.Firebase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class SensorDataCollector(private val sensorManager: SensorManager) : SensorEventListener {
     private var accelerometer: Sensor? = null
     private var gyroscope: Sensor? = null
     private lateinit var databaseReference: DatabaseReference
+
 
     init {
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -35,17 +39,26 @@ class SensorDataCollector(private val sensorManager: SensorManager) : SensorEven
 
     override fun onSensorChanged(event: SensorEvent) {
         val sensorType = event.sensor.type
+        val sensorName = event.sensor.name
         val sensorValues = event.values.joinToString(separator = ", ")
 
         val sensorData = hashMapOf(
             "sensor_type" to sensorType,
+            "sensor_name" to sensorName,
             "sensor_values" to sensorValues
         )
 
-        databaseReference.child("sensor_data").push().setValue(sensorData)
+        databaseReference.push().setValue(sensorData)
+            .addOnSuccessListener {
+                Log.d("Message","Sensor data added successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e("Message", "Error adding sensor data", e)
+            }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         // Ignorar
     }
+
 }
